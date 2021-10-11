5dfe551898bca86a891b06e41a2d87fc77c2aefd
Yohan Etifier <etifier.y@gmail.com>
2021-10-11 08:25:37 +0200

    refactoring header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,9 +4,8 @@ import { IconButton } from '@mui/material';
 import { AppBar, Toolbar } from '@mui/material';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
-/* import logo from '../../assets/logo.png' */
 import logo2 from '../../assets/logo2.png';
-/* import SearchIcon from '@mui/icons-material/Search'; */
+
 
 const ButtonOnAppBar = styled(IconButton)`
   position: relative;
@@ -20,35 +19,6 @@ const LogoContainer = styled.div`
     width: 25%; 
   }
 `;
-
-/* const SecondContainerOfMainContainer = styled.div`
-display: flex; 
-    align-items: center; 
-    height: 40px; 
-    width : 70%; 
-    border: 1px solid #CCCCCC; 
-    border-radius 3px; 
-    margin-left: 10px; 
-    
-    & > input {
-        width: 90%; 
-        margin-left: 10px; 
-        height: 30px; 
-        outline: none; 
-        border: none; 
-        
-    }
-    & > button {
-        background-color: #F8F8F8; 
-        width: 10%; 
-        height: 100%;
-        border-top: none; 
-        border-right: none; 
-        border-left: 1px solid #CCCCCC; 
-        border-bottom: none; 
-        cursor: pointer; 
-    }
-`; */
 
 export default function Header({ handleClick }) {
   return (
@@ -66,12 +36,6 @@ export default function Header({ handleClick }) {
           <LogoContainer>
             <img src={logo2} />
           </LogoContainer>
-          {/* <SecondContainerOfMainContainer>
-            <input placeholder="Rechercher" />
-            <button>
-              <SearchIcon />
-            </button>
-          </SecondContainerOfMainContainer> */}
         </Toolbar>
       </AppBar>
     </div>
